7f7b485932bf11811168aaadb5591a665bab681c
ANDRES EDUARDO RESTREPO FLOREZ <andres613@misena.edu.co>
2022-04-19 12:35:22 -0500

    Adding user register form

---
+++ app/src/Form/RegisterType.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class RegisterType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+
+        $builder->add('userName', TextType::class, array(
+            'label' => 'Nombre'
+        ))
+                ->add('email', EmailType::class, array(
+                    'label' => 'Correo electrónico'
+                ))
+                ->add('password', PasswordType::class, array(
+                    'label' => 'Contraseña'
+                ))
+                ->add('submit', SubmitType::class, array(
+                    'label' => 'Registrarse'
+                ));
+
+    }
+
+}
