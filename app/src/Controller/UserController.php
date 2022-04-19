4239c5b16ad46f3cb9cbe158292b613eabe38655
ANDRES EDUARDO RESTREPO FLOREZ <andres613@misena.edu.co>
2022-04-19 12:32:33 -0500

    Adding route and user controller

---
+++ app/src/Controller/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Form\RegisterType;
+
+class UserController extends AbstractController
+{
+    public function register(Request $request): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegisterType::class, $user);
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
